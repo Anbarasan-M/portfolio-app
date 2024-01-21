f96e354d4a392804c901585df222e050ed101ac3
Anbarasan <anbumuthu0509@gmail.com>
2024-01-21 12:32:35 +0530

    Rendered the app titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -13,3 +14,19 @@ with col2:
     My passion for continuous learning drives me to explore new horizons, whether it's through reading books or staying updated on the latest technologies. 
     As a junior Python developer, I'm excited to apply my skills and contribute to the dynamic field of IT with a focus on Python development."""
     st.info(content)
+
+content2 = """ This is my first portfolio app in python"""
+
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
